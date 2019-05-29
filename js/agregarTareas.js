0e5d9cdb3d0be3788b4e9c0005e257b034878c9e
Adder Orozco <a.j.orozco038@hotmail.com>
2019-05-29 12:23:46 -0600

    agregado eliminar y estilos.

--- js/agregarTareas.js
+++ js/agregarTareas.js
@@ -1,5 +1,7 @@
 (function() {
 
+
+
     var agregar = function() {
         var texto1 = document.getElementById("texto1").value;
         var contenido = document.createTextNode(texto1);
@@ -7,18 +9,34 @@
         var nuevo_nodo = document.createElement("div");
         nuevo_nodo.appendChild(contenido);
         nuevo_nodo.setAttribute("name", "tareas1");
+        nuevo_nodo.setAttribute("class", "tarea");
 
         var tareas = document.getElementsByName("tareas1");
-        var padre = tareas[0].parentNode;
-
         var referencia = tareas[0];
 
-
+        var padre = document.getElementById("contenedor1");
         padre.insertBefore(nuevo_nodo, referencia);
 
+        for (var i = 0; i <= seccion.children.length - 1; i++) {
+            seccion.children[i].addEventListener("click", eleminarTarea);
+            console.log(seccion.children[i]);
+        }
+    };
+
+    var eleminarTarea = function() {
+        this.parentNode.removeChild(this);
     };
 
     var boton1 = document.getElementById("boton1");
     boton1.addEventListener("click", agregar);
+
+    var seccion = document.getElementById("contenedor1");
+
+    for (var i = 0; i <= seccion.children.length - 1; i++) {
+        seccion.children[i].addEventListener("click", eleminarTarea);
+        console.log(seccion.children[i]);
+    }
+
+
 
 }())
