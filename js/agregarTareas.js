ae21be24d571783e685c0e6748c1e30842010b12
Adder Orozco <a.j.orozco038@hotmail.com>
2019-05-31 12:27:27 -0600

    subir y bajar pero con defecto al subir el primer elemento y al bajar el ultimo elemento.

--- js/agregarTareas.js
+++ js/agregarTareas.js
@@ -1,9 +1,17 @@
 (function() {
 
+    var tareas1 = document.getElementsByName("tareas1");
+    var contador = tareas1.length;
+    //console.log(contador);
+
+    var contador2 = 0;
+
     var agregar = function() {
 
         var texto1 = document.getElementById("texto1").value;
         var contenido = document.createTextNode(texto1);
+
+
 
 
         // crear la nueva tarea dentro de un div
@@ -13,16 +21,17 @@
 
         var nuevo_nodo = document.createElement("div");
         nuevo_nodo.appendChild(contenido);
-        nuevo_nodo.setAttribute("name", "tareas1");
         nuevo_nodo.setAttribute("class", "tarea");
+        nuevo_nodo.setAttribute("name", "tareas1");
+
 
         div1.appendChild(nuevo_nodo);
 
 
 
-
-
         //crear los botones para subir y bajar dentro de un div
+
+        contador++;
 
         var div2 = document.createElement("div");
         div2.setAttribute("name", "subcontenedor2");
@@ -30,11 +39,13 @@
 
         var botonSubir = document.createElement("button");
         botonSubir.innerHTML = "Subir";
-        botonSubir.setAttribute("id", "botonSubir");
+        botonSubir.setAttribute("name", "botonSubir");
+        botonSubir.setAttribute("id", "botonSubir" + contador);
 
         var botonBajar = document.createElement("button");
         botonBajar.innerHTML = "Bajar";
-        botonBajar.setAttribute("id", "botonBajar");
+        botonBajar.setAttribute("name", "botonBajar");
+        botonBajar.setAttribute("id", "botonBajar" + contador);
 
         div2.appendChild(botonSubir);
         div2.appendChild(botonBajar);
@@ -60,15 +71,78 @@
             tareas[i].addEventListener("click", eleminarTarea);
             //console.log(tareas[i]);
         }
+
+
+        for (var j = 0; j <= botonesSubir.length - 1; j++) {
+            botonesSubir[j].addEventListener("click", subirTarea);
+            //console.log(botonesSubir[i]);
+            contador2++;
+            //console.log(contador2);
+        }
+
+        for (var j = 0; j <= botonesBajar.length - 1; j++) {
+            botonesBajar[j].addEventListener("click", bajarTarea);
+            //console.log(botonesBajar[j]);
+        }
+        //console.log("************************");
     };
 
 
-
+    // funcion para eliminar las tareas
     var eleminarTarea = function() {
         subcontenedor1 = this.parentNode;
         conjunto = subcontenedor1.parentNode
         conjunto.parentNode.removeChild(conjunto);
     };
+
+    var subirTarea = function(e) {
+        padre = document.getElementById("contenedor1");
+        subcontenedor1 = this.parentNode;
+        conjunto = subcontenedor1.parentNode
+
+        //var nombreConjunto = conjunto.attributes["id"].value;
+        //var antes = document.getElementById(nombreConjunto).previousSibling
+
+        var antes = conjunto.previousSibling;
+
+        if (antes.nodeName === '#text') {
+            antes = antes.previousSibling
+        }
+
+        padre.insertBefore(conjunto, antes);
+    }
+
+    var bajarTarea = function(e) {
+        padre = document.getElementById("contenedor1");
+        subcontenedor1 = this.parentNode;
+        conjunto = subcontenedor1.parentNode
+
+        //var nombreConjunto = conjunto.attributes["id"].value;
+        //var antes = document.getElementById(nombreConjunto).previousSibling
+
+        var siguiente = conjunto.nextSibling;
+
+        if (siguiente.nodeName === '#text') {
+            siguiente = siguiente.nextSibling
+            console.log("vaciooooooo");
+        }
+
+        if (siguiente !== siguiente.lastChild) {
+            siguiente = siguiente.nextSibling;
+            console.log("ultimmmmmmmmmo");
+        }
+
+
+
+
+
+
+
+        padre.insertBefore(conjunto, siguiente);
+
+        //console.log(siguiente);
+        //console.log(padre);
+    }
 
 
     var boton1 = document.getElementById("boton1");
@@ -78,9 +152,26 @@
     //var conjuntos = document.getElementsByName("conjunto");
     var tareas = document.getElementsByName("tareas1");
 
+    //listar las tareas para enviarlas a funcion eliminar
     for (var i = 0; i <= tareas.length - 1; i++) {
         tareas[i].addEventListener("click", eleminarTarea);
-        //console.log(tareas[i]);
+    }
+
+
+    //listar los botones de SUBIR para enviarlas a funcion SUBIR
+    var botonesSubir = document.getElementsByName("botonSubir");
+
+    for (var j = 0; j <= botonesSubir.length - 1; j++) {
+        botonesSubir[j].addEventListener("click", subirTarea);
+    }
+
+
+    //listar los botones de BAJAR para enviarlas a funcion BAJAR
+    var botonesBajar = document.getElementsByName("botonBajar");
+
+    for (var j = 0; j <= botonesBajar.length - 1; j++) {
+        botonesBajar[j].addEventListener("click", bajarTarea);
+        //console.log(botonesBajar[j]);
     }
 
 
